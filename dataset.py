ba80251bd65c1078cfb079b4a6f41225aa9fffa0
xnuohz <xnuohz@126.com>
2021-05-15 15:45:36 +0000

    [feat] add preprocessing in dataset

--- dataset.py
+++ dataset.py
@@ -1,142 +1,201 @@
 import os
 import numpy as np
 import torch
-import dgl
-from collections import defaultdict
 from torch.utils.data import Dataset
-
-
-@profile
-def get_bbox(points):
-    r"""
-    
-    Description
-    -----------
-    Get the bounding box of a point cloud
-    
-    Parameters
-    ----------
-    points: torch.Tensor
-        [N, 3] matrix of input points
-    """
-    min_point = torch.min(points, 0)[0].data
-    max_point = torch.max(points, 0)[0].data
-    
-    return min_point, max_point
-
-
-@profile
-def grid_subsampling(points, feats, dl):
-    r"""
-
-    Description
-    -----------
-    Grid subsampling is implemented by C++ in the author's PyTorch code
-    
-    Parameters
-    ----------
-    points: torch.Tensor
-        [N, 3] matrix of input points
-    feats: torch.Tensor
-        [N, D] matrix of input features
-    dl: float
-        the size of grid voxels
-
-    Return
-    ----------
-    subsampled points and features
-    """
-    n_points = len(points)
-    min_corner, max_corner = get_bbox(points)
-    # is this nessesary?
-    min_corner = torch.floor(torch.div(min_corner, dl)) * dl
-    
-    sample_nx = int((max_corner[0] - min_corner[0]) / dl) + 1
-    sample_ny = int((max_corner[1] - min_corner[1]) / dl) + 1
-
-    data = defaultdict(list)
-
-    for p, f in zip(points, feats):
-        idx_x = int((p[0] - min_corner[0]) / dl)
-        idx_y = int((p[1] - min_corner[1]) / dl)
-        idx_z = int((p[2] - min_corner[2]) / dl)
-        idx = idx_x + sample_nx * idx_y + sample_nx * sample_ny * idx_z
-        data[idx].append(torch.cat([p, f]).view(1, -1))
-    
-    subsampled_data = []
-    
-    for _, v in data.items():
-        v = torch.cat(v)
-        subsampled_data.append(torch.mean(v, dim=0).view(1, -1))
-
-    subsampled_data = torch.cat(subsampled_data)
-
-    return subsampled_data[:, :3], subsampled_data[:, 3:]
-
-
-@profile
-def collate_fn(batch):
-    r"""
-        points: list, [b, n, 3]
-        feats: list, [b, n, d]
-        labels: list, [b,]
-        length: list, [b,], each value is the number of points in each point cloud
-    """
-    points, feats, labels, length = map(list, zip(*batch))
-    batch_points = torch.FloatTensor(np.concatenate(points))
-    batch_feats = torch.FloatTensor(np.concatenate(feats))
-    batch_labels = torch.LongTensor(labels).view(-1, 1)
-    batch_len = torch.LongTensor(length)
-    return batch_points, batch_feats, batch_labels, batch_len
+from utils import grid_subsampling, batch_neighbors, batch_grid_subsampling
 
 
 class ModelNet40Dataset(Dataset):
-    def __init__(self,
-                 root,
-                 dl,
-                 split='train',
-                 orient_correction=True,
-                 cache_size=10000):
+    def __init__(self, args, root, split='train'):
         assert split in ['train', 'test']
-        
+
+        self.config = args
         self.root = root
-        self.dl = dl
-        self.orient_correction = orient_correction
-        catfile = os.path.join(root, 'modelnet40_shape_names.txt')
+        self.split = split
+        catfile = os.path.join(root, 'modelnet10_shape_names.txt')
         cat = [l.rstrip() for l in open(catfile)]
         self.label_to_names = {k: v for k, v in enumerate(cat)}
         self.name_to_label = {v: k for k, v in self.label_to_names.items()}
-        
         # load point cloud
-        self.names = np.loadtxt(os.path.join(root, f'modelnet40_{split}.txt'), dtype=str)
-        self.cache_size = cache_size
-        self.cache = {}
+        self.points, self.feats, self.lengths, self.labels = self.load_subsampled_clouds()
 
     def __len__(self):
-        return len(self.names)
+        return len(self.labels)
 
-    @profile
     def __getitem__(self, idx):
-        if idx in self.cache:
-            return self.cache[idx]
-        else:
-            cloud_name = self.names[idx]
+        return 0
+    
+    def load_subsampled_clouds(self):
+        print(f'Loading {self.split} points subsampled at {self.config.first_subsampling_dl:.3f}')
+        filename = f'{self.root}/{self.split}_{self.config.first_subsampling_dl}_record.pkl'
+        
+        if os.path.exists(filename):
+            return torch.load(open(filename, 'rb'))
+        
+        names = np.loadtxt(f'{self.root}/modelnet10_{self.split}.txt', dtype=str)
+        
+        # Initialize containers
+        input_points = []
+        input_feats = []
+
+        # Advanced display
+        N = len(names)
+        progress_n = 30
+        fmt_str = '[{:<' + str(progress_n) + '}] {:5.1f}%'
+
+        # Collect point clouds
+        for i, cloud_name in enumerate(names):
+
+            # Read points
             class_folder = '_'.join(cloud_name.split('_')[:-1])
-            txt_file = f'{self.root}/{class_folder}/{cloud_name}.txt'
-            # point cloud
+            txt_file = os.path.join(self.root, class_folder, cloud_name) + '.txt'
             data = np.loadtxt(txt_file, delimiter=',', dtype=np.float32)
             data = torch.FloatTensor(data)
-            points, feats = grid_subsampling(points=data[:, :3],
-                                             feats=data[:, 3:],
-                                             dl=self.dl)
 
-            if self.orient_correction:
-                points = points[:, [0, 2, 1]]
-                feats = feats[:, [0, 2, 1]]
-            # ground truth
-            label = self.name_to_label[class_folder]
+            # Subsample them
+            # Assume self.config.first_subsampling_dl > 0
+            points, feats = grid_subsampling(data[:, :3],
+                                                self.config.first_subsampling_dl,
+                                                data[:, 3:])
 
-            if len(self.cache) < self.cache_size:
-                self.cache[idx] = (points, feats, label, len(points))
+            print('', end='\r')
+            print(fmt_str.format('#' * ((i * progress_n) // N), 100 * i / N), end='', flush=True)
 
-            return points, feats, label, len(points)
+            # Add to list
+            input_points += [points]
+            input_feats += [feats]
+
+            if i == 5:
+                break
+
+        print('', end='\r')
+        print(fmt_str.format('#' * progress_n, 100), end='', flush=True)
+        print()
+
+        # Get labels
+        label_names = ['_'.join(name.split('_')[:-1]) for name in names]
+        input_labels = torch.LongTensor([self.name_to_label[name] for name in label_names])
+
+        # convert to torch.Tensor
+        lengths = torch.LongTensor([p.shape[0] for p in input_points])
+        input_points = torch.cat(input_points, dim=0)
+        input_feats = torch.cat(input_feats, dim=0)
+        
+        # Save for later use
+        torch.save((input_points,
+                    input_feats,
+                    lengths,
+                    input_labels), filename)
+
+        return input_points, input_feats, lengths, input_labels
+
+    def classification_inputs(self,
+                              stacked_points,
+                              stacked_features,
+                              labels,
+                              stacked_lengths):
+        
+        # Starting radius of convolutions
+        r_normal = self.config.first_subsampling_dl * self.config.conv_radius
+
+        # Starting layer
+        layer_blocks = []
+
+        # Lists of inputs
+        input_points = []
+        input_neighbors = []
+        input_pools = []
+        input_stack_lengths = []
+
+        ######################
+        # Loop over the blocks
+        ######################
+
+        # simple -> resnetb -> resnetb_strided -> resnetb -> resnetb_strided -> resnetb -> global_average
+        arch = self.config.architecture
+
+        for block_i, block in enumerate(arch):
+
+            # Get all blocks of the layer
+            if not ('strided' in block or 'global' in block):
+                layer_blocks += [block]
+                continue
+
+            # Stop when meeting a global pooling
+            if 'global' in block:
+                break
+
+            # Convolution neighbors indices
+            # *****************************
+
+            if layer_blocks:
+                # Convolutions are done in this layer, compute the neighbors with the good radius
+                conv_src, conv_dst = batch_neighbors(stacked_points,
+                                                     stacked_points,
+                                                     stack_lengths,
+                                                     stacked_lengths,
+                                                     r_normal)
+            else:
+                # This layer only perform pooling, no neighbors required
+                conv_src = np.zeros((0, 1), dtype=np.int32)
+                conv_dst = np.zeros((0, 1), dtype=np.int32)
+
+            # Pooling neighbors indices
+            # *************************
+
+            # If end of layer is a pooling operation
+            if 'strided' in block:
+
+                # New subsampling length
+                dl = 2 * r_normal / self.config.conv_radius
+
+                # Subsampled points
+                pool_p, pool_b = batch_grid_subsampling(stacked_points, stack_lengths, dl)
+
+                # Subsample indices
+                pool_src, pool_dst = batch_neighbors(pool_p,
+                                                     stacked_points,
+                                                     pool_b,
+                                                     stack_lengths,
+                                                     r_normal)
+
+            else:
+                # No pooling in the end of this layer, no pooling indices required
+                pool_i = np.zeros((0, 1), dtype=np.int32)
+                pool_p = np.zeros((0, 1), dtype=np.float32)
+                pool_b = np.zeros((0,), dtype=np.int32)
+
+            # Updating input lists
+            input_points += [stacked_points]
+            input_neighbors += [(conv_src, conv_dst)]
+            input_pools += [(pool_src, pool_dst)]
+            input_stack_lengths += [stack_lengths]
+
+            # New points for next layer
+            stacked_points = pool_p
+            stack_lengths = pool_b
+
+            # Update radius and reset blocks
+            r_normal *= 2
+            layer_blocks = []
+
+        ###############
+        # Return inputs
+        ###############
+
+        # list of network inputs
+        li = input_points + input_neighbors + input_pools + input_stack_lengths
+        li += [stacked_features, labels]
+
+        return li
+
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser(description='KPConv')
+    parser.add_argument('--first_subsampling_dl', type=float, default=0.02)
+    args = parser.parse_args()
+
+    dataset = ModelNet40Dataset(args, 'data/ModelNet40')
+    print(len(dataset))
--- utils.py
+++ utils.py
@@ -1,194 +1,194 @@
-import os
-import numpy as np
-import matplotlib.pyplot as plt
+import torch
+from collections import defaultdict
 
 
-def kernel_point_optimization_debug(radius, num_points, num_kernels=1, dimension=3,
-                                    fixed='center', ratio=0.66, verbose=0):
-    """
-    From the author's code
-    Creation of kernel point via optimization of potentials.
-    :param radius: Radius of the kernels
-    :param num_points: points composing kernels
-    :param num_kernels: number of wanted kernels
-    :param dimension: dimension of the space
-    :param fixed: fix position of certain kernel points ('none', 'center' or 'verticals')
-    :param ratio: ratio of the radius where you want the kernels points to be placed
-    :param verbose: display option
-    :return: points [num_kernels, num_points, dimension]
-    """
+def square_distance(src, dst):
+    r"""
 
-    #######################
-    # Parameters definition
-    #######################
+    Description
+    -----------
+    Computes distances for src points -> dst points
 
-    # Radius used for optimization (points are rescaled afterwards)
-    radius0 = 1
-    diameter0 = 2
-
-    # Factor multiplicating gradients for moving points (~learning rate)
-    moving_factor = 1e-2
-    continuous_moving_decay = 0.9995
-
-    # Gradient threshold to stop optimization
-    thresh = 1e-5
-
-    # Gradient clipping value
-    clip = 0.05 * radius0
-
-    #######################
-    # Kernel initialization
-    #######################
-
-    # Random kernel points
-    kernel_points = np.random.rand(num_kernels * num_points - 1, dimension) * diameter0 - radius0
-    while (kernel_points.shape[0] < num_kernels * num_points):
-        new_points = np.random.rand(num_kernels * num_points - 1, dimension) * diameter0 - radius0
-        kernel_points = np.vstack((kernel_points, new_points))
-        d2 = np.sum(np.power(kernel_points, 2), axis=1)
-        kernel_points = kernel_points[d2 < 0.5 * radius0 * radius0, :]
-    kernel_points = kernel_points[:num_kernels * num_points, :].reshape((num_kernels, num_points, -1))
-
-    # Optionnal fixing
-    if fixed == 'center':
-        kernel_points[:, 0, :] *= 0
-    if fixed == 'verticals':
-        kernel_points[:, :3, :] *= 0
-        kernel_points[:, 1, -1] += 2 * radius0 / 3
-        kernel_points[:, 2, -1] -= 2 * radius0 / 3
-
-    #####################
-    # Kernel optimization
-    #####################
-
-    # Initialize figure
-    if verbose>1:
-        fig = plt.figure()
-
-    saved_gradient_norms = np.zeros((10000, num_kernels))
-    old_gradient_norms = np.zeros((num_kernels, num_points))
-    step = -1
-    while step < 10000:
-
-        # Increment
-        step += 1
-
-        # Compute gradients
-        # *****************
-
-        # Derivative of the sum of potentials of all points
-        A = np.expand_dims(kernel_points, axis=2)
-        B = np.expand_dims(kernel_points, axis=1)
-        interd2 = np.sum(np.power(A - B, 2), axis=-1)
-        inter_grads = (A - B) / (np.power(np.expand_dims(interd2, -1), 3/2) + 1e-6)
-        inter_grads = np.sum(inter_grads, axis=1)
-
-        # Derivative of the radius potential
-        circle_grads = 10*kernel_points
-
-        # All gradients
-        gradients = inter_grads + circle_grads
-
-        if fixed == 'verticals':
-            gradients[:, 1:3, :-1] = 0
-
-        # Stop condition
-        # **************
-
-        # Compute norm of gradients
-        gradients_norms = np.sqrt(np.sum(np.power(gradients, 2), axis=-1))
-        saved_gradient_norms[step, :] = np.max(gradients_norms, axis=1)
-
-        # Stop if all moving points are gradients fixed (low gradients diff)
-
-        if fixed == 'center' and np.max(np.abs(old_gradient_norms[:, 1:] - gradients_norms[:, 1:])) < thresh:
-            break
-        elif fixed == 'verticals' and np.max(np.abs(old_gradient_norms[:, 3:] - gradients_norms[:, 3:])) < thresh:
-            break
-        elif np.max(np.abs(old_gradient_norms - gradients_norms)) < thresh:
-            break
-        old_gradient_norms = gradients_norms
-
-        # Move points
-        # ***********
-
-        # Clip gradient to get moving dists
-        moving_dists = np.minimum(moving_factor * gradients_norms, clip)
-
-        # Fix central point
-        if fixed == 'center':
-            moving_dists[:, 0] = 0
-        if fixed == 'verticals':
-            moving_dists[:, 0] = 0
-
-        # Move points
-        kernel_points -= np.expand_dims(moving_dists, -1) * gradients / np.expand_dims(gradients_norms + 1e-6, -1)
-
-        if verbose:
-            print('step {:5d} / max grad = {:f}'.format(step, np.max(gradients_norms[:, 3:])))
-        if verbose > 1:
-            plt.clf()
-            plt.plot(kernel_points[0, :, 0], kernel_points[0, :, 1], '.')
-            circle = plt.Circle((0, 0), radius, color='r', fill=False)
-            fig.axes[0].add_artist(circle)
-            fig.axes[0].set_xlim((-radius*1.1, radius*1.1))
-            fig.axes[0].set_ylim((-radius*1.1, radius*1.1))
-            fig.axes[0].set_aspect('equal')
-            plt.draw()
-            plt.pause(0.001)
-            plt.show(block=False)
-            print(moving_factor)
-
-        # moving factor decay
-        moving_factor *= continuous_moving_decay
-
-    # Remove unused lines in the saved gradients
-    if step < 10000:
-        saved_gradient_norms = saved_gradient_norms[:step+1, :]
-
-    # Rescale radius to fit the wanted ratio of radius
-    r = np.sqrt(np.sum(np.power(kernel_points, 2), axis=-1))
-    kernel_points *= ratio / np.mean(r[:, 1:])
-
-    # Rescale kernels with real radius
-    return kernel_points * radius, saved_gradient_norms
-
-
-def load_kernels(radius, kernel_size, p_dim, fixed):
-    kernel_dir = 'kernels'
-
-    if not os.path.exists(kernel_dir):
-        os.makedirs(kernel_dir)
+    Parameters
+    -----------
+    src: torch.Tensor
+        [N, 3]
+    dst: torch.Tensor
+        [N, 3]
     
-    kernel_file = f'{kernel_dir}/kp_{kernel_size}_{p_dim}_{radius}_{fixed}.npy'
+    Return
+    -----------
+    dist: torch.Tensor
+        [N, N]
+    """
+    N, _ = src.shape
+    M, _ = dst.shape
+    dist = -2 * torch.matmul(src, dst.permute(1, 0))
+    dist += torch.sum(src ** 2, -1).view(N, 1)
+    dist += torch.sum(dst ** 2, -1).view(1, M)
+    return dist
 
-    if not os.path.exists(kernel_file):
-        # Create kernels
-        kernel_points, grad_norms = kernel_point_optimization_debug(1.0,
-                                                                    kernel_size,
-                                                                    num_kernels=100,
-                                                                    dimension=p_dim,
-                                                                    fixed=fixed)
-        # Find best candidate
-        best_k = np.argmin(grad_norms[-1, :])
 
-        # Save points
-        kernel_points = kernel_points[best_k, :, :]
-        np.save(kernel_file, kernel_points)
+def batch_neighbors(queries,
+                    supports,
+                    q_batches,
+                    s_batches,
+                    radius):
+    r"""
+
+    Description
+    -----------
+    Computes neighbors for a batch of point clouds
+    
+    Parameters
+    -----------
+    queries: torch.Tensor
+        [N, 3], batched point clouds(center node)
+    supports: torch.Tensor
+        [N, 3], batched point clouds(neighbors set)
+    q_batches: torch.Tensor
+        [B,] the list of lengths of batched point clouds
+    s_batches: torch.Tensor
+        [B,] the list of lengths of batched point clouds
+    radius: float32
+
+    Return
+    -----------
+    stacked_src: list
+        [[N1,], [N2,], ...]
+    stacked_dst: list
+        [[N1,], [N2,], ...]
+    """
+    
+    stacked_src, stacked_dst = [], []
+    
+    for i in range(len(q_batches) - 1):
+        query_cloud = queries[int(q_batches[i]):int(q_batches[i + 1])]  # [N, 3]
+        support_cloud = supports[int(s_batches[i]):int(s_batches[i + 1])]  # [M, 3]
+        N, M = len(query_cloud), len(support_cloud)
+        # get neighbors for each point
+        dists = square_distance(query_cloud, support_cloud)  # [N, M]
+        group_idx = torch.arange(M, dtype=torch.long).view(1, -1).repeat(N, 1)
+        group_idx[dists > self.radius ** 2] = M
+        # get edges idx
+        src, dst = torch.where(group_idx != num_points)
+        stacked_src.append(src)
+        stacked_dst.append(dst)
+    
+    return stacked_src, stacked_dst
+
+
+def get_bbox(points):
+    r"""
+    
+    Description
+    -----------
+    Get the bounding box of a point cloud
+    
+    Parameters
+    ----------
+    points: torch.Tensor
+        [N, 3] matrix of input points
+    """
+    min_point = torch.min(points, 0)[0].data
+    max_point = torch.max(points, 0)[0].data
+    
+    return min_point, max_point
+
+
+def grid_subsampling(points, dl, features=None):
+    r"""
+    Description
+    -----------
+    Grid subsampling is implemented by C++ in the author's PyTorch code
+    
+    Parameters
+    ----------
+    points: torch.Tensor
+        [N, 3] matrix of input points
+    dl: float
+        the size of grid voxels
+    features: torch.Tensor
+        [N, d] matrix of input features
+
+    Return
+    ----------
+    subsampled points and features
+    """
+    n_points = len(points)
+    min_corner, max_corner = get_bbox(points)
+    # is this nessesary?
+    min_corner = torch.floor(torch.div(min_corner, dl)) * dl
+    
+    sample_nx = int((max_corner[0] - min_corner[0]) / dl) + 1
+    sample_ny = int((max_corner[1] - min_corner[1]) / dl) + 1
+
+    data = defaultdict(list)
+
+    if features is not None:
+        points = torch.cat([points, features], dim=-1)
+
+    for p in points:
+        idx_x = int((p[0] - min_corner[0]) / dl)
+        idx_y = int((p[1] - min_corner[1]) / dl)
+        idx_z = int((p[2] - min_corner[2]) / dl)
+        idx = idx_x + sample_nx * idx_y + sample_nx * sample_ny * idx_z
+        data[idx].append(p.view(1, -1))
+    
+    subsampled_data = []
+    
+    for _, v in data.items():
+        v = torch.cat(v)
+        subsampled_data.append(torch.mean(v, dim=0).view(1, -1))
+
+    subsampled_data = torch.cat(subsampled_data)
+
+    if features is not None:
+        return subsampled_data[:, :3], subsampled_data[:, 3:]
     else:
-        kernel_points = np.load(kernel_file)
+        return subsampled_data
+
+
+def batch_grid_subsampling(stacked_points, stack_lengths, dl, offset=5):
+    r"""
     
-    # Random rorations for the kernel
-    R = np.eye(p_dim)
-    theta = np.random.rand() * 2 * np.pi
-    # currently we only support that fixed is 'center', p_dim is 3 and kernel_size <= 30
-    c, s = np.cos(theta), np.sin(theta)
-    R = np.array([[c, -s, 0], [s, c, 0], [0, 0, 1]], dtype=np.float32)
-    # Add a small noise
-    kernel_points = kernel_points + np.random.normal(scale=0.01, size=kernel_points.shape)
-    # Scale kernels
-    kernel_points = radius * kernel_points
-    # Rotate kernels
-    kernel_points = np.matmul(kernel_points, R)
+    Description
+    -----------
+    Create barycenters from batched points for the next layer by batch grid subsampling
+
+    Parameters
+    -----------
+    stacked_points: torch.Tensor
+        [N, 3], batched point clouds
+    stack_lengths: torch.Tensor
+        [B,] the list of lengths of batched point clouds
+    dl: float32
+        the size of grid voxels
+
+    Return
+    -----------
+    pool_p: torch.Tensor
+        [[N1, 3], [N2, 3], ...]
+    pool_b: torch.Tensor
+        [N1, N2, ...] 
+    """
+    # assume that stacked_points, stack_lengths are on CPU before training
+    # +offset -> grid subsampling simultaneously -> -offset
+    offsets = torch.arange(0, len(stack_lengths) * offset, offset)
+    # each offset will be repeated by the number of each point cloud
+    stacked_offsets = offsets.repeat_interleave(stack_lengths).reshape(-1, 1)  # [batch, 1]
+    stacked_offset_points = stacked_points + stacked_offsets  # [batch, 3]
     
-    return kernel_points
+    pool_points = grid_subsampling(stacked_offset_points, dl)
+    # calculate pool batch length
+    tmp_points = torch.cat([pool_points, torch.zeros(1, pool_points.size()[1])], dim=0)
+    # assume that there exists a gap between each point cloud
+    gap = torch.abs(tmp_points[1:, :] - tmp_points[:-1, :]) >= offset - 2
+    pool_cumsum_batch = torch.cat([torch.zeros(1), torch.where(gap[:, 0] == True)[0] + 1])
+    pool_batch_len = (pool_cumsum_batch[1:] - pool_cumsum_batch[:-1]).long()
+    # back to the origin scale
+    pool_offsets = torch.arange(0, len(pool_batch_len) * offset, offset)
+    pool_batch_offsets = pool_offsets.repeat_interleave(pool_batch_len).reshape(-1, 1)
+    pool_points = pool_points - pool_batch_offsets
+
+    return pool_points, pool_batch_len
